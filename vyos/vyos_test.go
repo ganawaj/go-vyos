d053fe1f51258dbff79d7992336c6604a2dacaeb
Ganawa Juanah <ganawa@juanah.com>
2024-11-18 16:56:37 -0600

    add show test

--- vyos/vyos_test.go
+++ vyos/vyos_test.go
@@ -1,6 +1,7 @@
 package vyos
 
 import (
+	"net/http"
 	"testing"
 )
 
@@ -18,4 +19,54 @@ func TestNewClient(t *testing.T) {
 	if c.client == c2.client {
 		t.Error("NewClient returned same http.Clients, but they should differ")
 	}
+
+}
+
+// TestClientCopy tests the copy method of the Client struct.
+func TestClientCopy(t *testing.T) {
+
+	t.Parallel()
+	c := NewClient(nil)
+	c2 := c.copy()
+
+	if c.client == c2.client {
+		t.Error("Client copy returned same http.Clients, but they should differ")
+	}
+
+}
+
+// TestClientWithURL tests the WithURL method of the Client struct.
+func TestClientWithURL(t *testing.T) {
+
+	t.Parallel()
+	c := NewClient(nil).WithURL("https://test.com")
+
+	if got, want := c.BaseURL, "https://test.com"; got != want {
+		t.Errorf("Client WithURL BaseURL is %v, want %v", got, want)
+	}
+}
+
+func TestClientWithToken(t *testing.T) {
+
+	t.Parallel()
+	c := NewClient(nil).WithToken("test")
+
+	if got, want := c.Token, "test"; got != want {
+		t.Errorf("Client WithToken Token is %v, want %v", got, want)
+	}
+}
+
+func TestClientInsecure(t *testing.T) {
+
+	t.Parallel()
+	c := NewClient(nil).Insecure()
+
+	if c.client.Transport == nil {
+		t.Error("Client Insecure Transport is nil, but it should be set")
+	}
+
+	if c.client.Transport.(*http.Transport).TLSClientConfig.InsecureSkipVerify != true {
+		t.Error("Client Insecure InsecureSkipVerify is false, but it should be true")
+	}
+
 }
